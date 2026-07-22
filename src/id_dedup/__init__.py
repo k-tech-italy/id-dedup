11b8d5b236cc991e32b584981f2e851d6f29bb00
Diego Paolicelli <d.paolicelli@k-tech.it>
2026-07-22 17:44:16 +0200

    feat: add Celery

--- src/id_dedup/__init__.py
+++ src/id_dedup/__init__.py
@@ -1,5 +1,8 @@
-__author__ = 'KTech'
-__version__ = '0.1.0'
-__name__ = 'id-dedup'
+from .celery import app as celery_app
+
+__author__ = "KTech"
+__version__ = "0.1.0"
+__name__ = "id-dedup"
 VERSION = __version__
 NAME = __name__
+__all__ = ("celery_app",)
