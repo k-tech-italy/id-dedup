f6736a904d8db16ce31f0eaa9d638e90d3313e4b
Diego Paolicelli <d.paolicelli@k-tech.it>
2026-07-23 14:46:42 +0200

    chore: do not shadow built-in `__name__` in root package

--- src/id_dedup/__init__.py
+++ src/id_dedup/__init__.py
@@ -2,7 +2,6 @@ from .celery import app as celery_app
 
 __author__ = "KTech"
 __version__ = "0.1.0"
-__name__ = "id-dedup"
+PACKAGE_NAME = "id-dedup"
 VERSION = __version__
-NAME = __name__
 __all__ = ("celery_app",)
