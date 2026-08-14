c35fa22c665aea602f8913197d49b93b8c615a43
Diego Paolicelli <d.paolicelli@k-tech.it>
2026-08-14 12:31:20 +0200

    test(workflow): clean up direct factory usages

--- tests/integration/workflow/test_upload_view.py
+++ tests/integration/workflow/test_upload_view.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db import IntegrityError
-from model_bakery import baker
 
 from id_dedup.workflow.models import Batch, Image, OutboxMessage
 from id_dedup.workflow.service import EmptyBatch, register_upload
@@ -142,10 +141,11 @@ class TestUploadView:
     # Constraints
     # ------------------------------------------------------------------
 
-    def test_duplicate_source_image_rejected(self, batch):
-        baker.make(Image, batch=batch, source_image="images/collision.jpg")
+    def test_duplicate_source_image_rejected(self, image_factory):
+        # NOTE: this is testing image creation behavior
+        image_factory(source_image="images/collision.jpg")
         with pytest.raises(IntegrityError):
-            baker.make(Image, batch=batch, source_image="images/collision.jpg")
+            image_factory(source_image="images/collision.jpg")
 
 
 @pytest.mark.django_db
